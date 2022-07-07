bd5de6cf102100cca727687d6b1069130b0de5bc
iespinoza96 <iespinoza@digis01.com>
2022-07-07 11:54:44 -0500

    Web API MVC

--- DL/LEscogidoGenMayoContext.cs
+++ DL/LEscogidoGenMayoContext.cs
@@ -27,7 +27,7 @@ namespace DL
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=LAPTOP-RH1B3NSI ; Database= LEscogidoGenMayo; Trusted_Connection=True; User ID=sa; Password=pass@word1;");
+                optionsBuilder.UseSqlServer("Server=LAPTOP-VA31VKK7; Database= LEscogidoGenMayo; Trusted_Connection=True; User ID=sa; Password=pass@word1;");
             }
         }
 
