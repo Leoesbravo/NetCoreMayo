2fab0febefbac5d014d8e2a9ac9ddac25a464b10
iespinoza96 <iespinoza@digis01.com>
2022-07-08 11:38:40 -0500

    Login

---
+++ PL/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PL.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            ML.Materia materia = new ML.Materia();
+
+            return View(materia);
+        }
+        [HttpPost]
+        public IActionResult Login(ML.Materia materia)
+        {
+
+            return RedirectToAction("Home");
+
+        }
+    }
+}
